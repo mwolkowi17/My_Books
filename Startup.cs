2a83449354e4f5c56b6c986c9346581a95fb5601
Marcin Wołkowicz <mwolkowi17@hotmail.com>
2019-10-08 21:01:43 +0200

    Conf1

--- Startup.cs
+++ Startup.cs
@@ -27,20 +27,22 @@ namespace MyBooks
         {
             services.AddRazorPages();
 
+            /*services.AddDbContext<MyBooksContext>(options =>
+                  options.UseSqlServer(Configuration.GetConnectionString("MyBooksContext")));*/
             // Use SQL Database if in Azure, otherwise, use SQLite
-if(Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
-    services.AddDbContext<MyDatabaseContext>(options =>
-            options.UseSqlServer(Configuration.GetConnectionString("MyDbConnection")));
-else
-    services.AddDbContext<MyDatabaseContext>(options =>
-            options.UseSqlite("Data Source=localdatabase.db"));
+            if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
+                services.AddDbContext<marcinucha1>(options =>
+                        options.UseSqlServer(Configuration.GetConnectionString("marcinucha1")));
+            else
+                services.AddDbContext<MyBooksContext>(options =>
+                  options.UseSqlServer(Configuration.GetConnectionString("MyBooksContext"))); ;
 
-// Automatically perform database migration
-services.BuildServiceProvider().GetService<MyDatabaseContext>().Database.Migrate();
+            // Automatically perform database migration
+            services.BuildServiceProvider().GetService<marcinucha1>().Database.Migrate();
+
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+            // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+            public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
